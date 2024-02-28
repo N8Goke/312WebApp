fc7a01ff90b403a1a5decb68f84d9dee8a45c866
dwu35 <dwu35@buffalo.edu>
2024-02-28 17:13:19 -0500

    add nosniff and basic css

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html") 
 
-
+@app.after_request
+def add_header(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
