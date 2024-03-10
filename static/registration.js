cb13560d954c4ec3bdc740e1be6d81809dbd35ec
PossiblyAHuman <112639806+PossiblyAHuman@users.noreply.github.com>
2024-03-10 01:35:33 -0500

    fixed registration header

--- static/registration.js
+++ static/registration.js
@@ -1,16 +1,17 @@
 function register()
 {
-    const request = new XMLHttpRequest();
+    request = new XMLHttpRequest();   
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             console.log(this.response);
         }
     }
     var username = document.getElementById("username").value
-    var password = document.getAnimations("password").value
+    var password = document.getElementById("password").value
 
     data = {"username": username, "password": password}
-    request.open("POST", "../app.py")
+    request.open("POST", "/registrationServer")
+    request.setRequestHeader('Content-Type', 'application/json')
     request.send(JSON.stringify(data))
 
 }
